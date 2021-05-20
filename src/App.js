519a7a9f1cf0337a5f50297755de385a6586b21e
Lau Kin Peng <kinpenglau@kinpenglau.ifca.com.my>
2021-05-20 18:19:59 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,34 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert'
+import About from './components/pages/About'
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
-  async componentDidMount () {
-    console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET)
-    this.setState({loading: true});
+  // async componentDidMount () {
+  //   console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET)
+  //   this.setState({loading: true});
 
-    const res = await axios.get(`https://api.github.com/users?
+  //   const res = await axios.get(`https://api.github.com/users?
+  //   client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+  //   client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+
+  //   this.setState({users: res.data, loading: false})
+  // }
+
+  // search github users
+  searchUsers = async text => {
+    this.setState({ loading: true })
+
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&
     client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
     client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
 
-    this.setState({users: res.data, loading: false})
+    this.setState({users: res.data.items, loading: false})
+  }
+
+  // clear users from state
+  clearUsers = () => this.setState({ users: [], loading:false })
+
+  // Set Alert 
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type }})
+
+    setTimeout(() => this.setState({ alert: null }), 5000)
   }
 
   render() {
     return (
-      <div className='App'>
-        <Navbar/>
-        <div className='container'>
-          <Users loading={this.state.loading} users={this.state.users}/>
-        </div>
-      </div>
+      <Router>
+        <div className='App'>
+          <Navbar/>
+          <div className='container'>
+            <Alert alert={this.state.alert}/>
+            <Switch>
+              <Route exact path='/' render={props => (
+                  <Fragment>
+                    <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length > 0 ? true : false } setAlert={this.setAlert}/>
+                    <Users loading={this.state.loading} users={this.state.users}/>
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About}/>
+            </Switch>
+          </div>
+       </div>
+      </Router>
     );
   }
 }
